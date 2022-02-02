bc8de49967d339324f2b3dfcf929752ea04b2c17
muni77-sh <74359899+muni77-sh@users.noreply.github.com>
2022-02-02 15:37:07 +0530

    updated the file

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -1,13 +1,13 @@
-package com.jhooq.Jhooqk8s.ws;
+package com.muni.munik8s.ws;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class JhooqDockerDemoController {
+public class muniDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello - Jhooq-k8s i Have updated the message";
+        return "Hello - muni-k8s i Have updated the message";
     }
 }
